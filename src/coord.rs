dc320a51790ad9ba0ec2b9499803685200957ff0
Hidekazu Kobayashi <hidekazu-kobayashi@cookpad.com>
2018-03-28 00:43:02 +0900

    implement AddAssign::add_assign for Coord

--- src/coord.rs
+++ src/coord.rs
@@ -17,7 +17,10 @@ impl Add for Coord {
 /// `+=` 演算子のオーバーロード
 impl AddAssign for Coord {
     fn add_assign(&mut self, rhs: Self) {
-        unimplemented!();
+        let &mut Coord(ref mut x1, ref mut y1) = self;
+        let Coord(x2, y2) = rhs;
+        *x1 += x2;
+        *y1 += y2;
     }
 }
 const X_AXIS: [char; 8] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
